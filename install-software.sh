50870d273d202320030ab0e59b290274df2cac40
David Rotert <dr848921@fh-muenster.de>
2025-08-31 21:48:07 +0200

    Configure Docker for private network

--- install-software.sh
+++ install-software.sh
@@ -14,7 +14,33 @@ echo \
     | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install docker-ce docker-ce-rootless-extras docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+docker_config="$(cat <<EOF
+{
+    "default-address-pools": [
+        {
+            "base":"172.17.0.0/16",
+            "size": 24
+        }
+    ]
+}
+EOF
+)"
+sudo mkdir -p /etc/docker
+echo "$docker_config" | sudo tee > /etc/docker/daemon.json
 dockerd-rootless-setuptool.sh install
+docker_rootless_config="$(cat <<EOF
+{
+    "default-address-pools": [
+        {
+            "base":"172.18.0.0/16",
+            "size": 24
+        }
+    ]
+}
+EOF
+)"
+sudo mkdir -p "$HOME/.config/docker"
+echo "$docker_rootless_config" | sudo tee > "$HOME/.config/docker/daemon.json"
 
 # Install virtualization tools
 sudo apt install virt-manager
